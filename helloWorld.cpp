fed058a2ad9760a5bef4d2d81f6d1e07d09e8da5
Omar Del Toro <19033@portalcedi.edu.mx>
2024-01-30 13:42:29 -0600

    add edad

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -3,6 +3,10 @@ using namespace std;
 
 int main()
 {
+    int edad;
     cout << "hola mundo" << endl;
+    cout << "Ingrese su edad" << endl;
+    cin >> edad;
+    cout << edad << endl;
     return 0;
 }
